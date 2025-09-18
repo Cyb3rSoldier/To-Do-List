a97af7dec51d111343f34fc7d114e5a24f9608bc
Omar Faruk Rakib <omarfarukrakib26@gmail.com>
2025-09-18 14:57:04 +0600

    Task list added

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To-Do List</title>
+    <link rel="stylesheet" href="design.css">
+</head>
+<body>
+    <div class="container">
+        <h1>To-DO List</h1>
+        <form action="" method="post">
+            <input type="text" name="task" placeholder="Enter a new Task">
+            <button type="submit" name= "addtask">ADD</button>
+        </form>
+    </div>
+</body>
+</html>
